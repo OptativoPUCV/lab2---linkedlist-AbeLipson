c71cb6c0435fdf060180d3d86d0499aacf5b3370
Replit user <>
2023-08-24 20:26:48 +0000

    new try

--- list.c
+++ list.c
@@ -47,7 +47,9 @@ void *firstList(List *list) {
 }
 
 void *nextList(List *list) {
-
+  if (list->current->next == NULL || list->current == NULL) {
+    return NULL; // Return NULL if the list is empty or invalid
+  }
   list->current = list->current->next;
   return list->current->data;
 }
